9d15149e2c7231441926770e43c0a0b52a0b974a
Valentin Haenel <valentin.haenel@gmx.de>
2012-02-09 21:46:30 +0100

    proper parsing from config file

--- pykarotz.py
+++ pykarotz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import hmac
 import urllib
 import time
@@ -6,10 +7,9 @@ import random
 import hashlib
 import base64
 import lxml.etree as le
+import ConfigParser
 
-APIKEY= 'APIKEY'
-SECRET= 'SECRET'
-INSTALLID = 'INSTALLID'
+
 
 # sign parameters in alphabetical order
 def sign(parameters, signature):
@@ -22,11 +22,43 @@ def sign(parameters, signature):
     query = query + "&signature=" + urllib.quote(signValue)
     return query
 
-parameters = {}
-parameters['installid'] = INSTALLID
-parameters['apikey'] = APIKEY
-parameters['once'] = "%d" % random.randint(100000000, 99999999999)
-parameters['timestamp'] = "%d" % time.time()
+
+def parse_config(config_filename=None):
+    """ Parse a configuration file with app settings.
+
+    Parameters
+    ----------
+    config_filename : str
+        the name of the config file
+
+    Returns
+    -------
+    settings : dict
+        the settings, values for 'apikey', 'secret' and 'installid'
+
+    Raises
+    ------
+    IOError:
+        if config_filename does not exist
+    NoSectionError
+        if no section 'karotz-app-config' exists
+    NoOptionError
+        if any one of 'apikey', 'secret' and 'installid' does not exist
+
+    """
+    # use the config file if none given
+    if not config_filename:
+        config_filename = os.path.expanduser("~/.pykarotz")
+    # parse the config
+    cp = ConfigParser.RawConfigParser()
+    with open(config_filename) as fp:
+        cp.readfp(fp)
+    # convert to dict and return
+    # doing it this way, will raise exceptions if the section or option doesn't
+    # exist
+    section = 'karotz-app-config'
+    return dict((setting, cp.get(section, setting))
+            for setting in ['apikey', 'secret', 'installid'])
 
 def start():
     query = sign(parameters, SECRET)
