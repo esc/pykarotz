1d6042094c60153a481f92ae5cb5cd24027cb5a2
Valentin Haenel <valentin.haenel@gmx.de>
2012-02-26 01:03:39 +0100

    pylint format

--- karotz.py
+++ karotz.py
@@ -223,7 +223,7 @@ class Karotz(object):
         self.ears(reset=True)
 
     def sad(self):
-        self.ears(left=23 ,right=23, relative=False)
+        self.ears(left=23, right=23, relative=False)
 
     def led_light(self, color='FFFFFF'):
         rest_call('led', {'action': 'light',
